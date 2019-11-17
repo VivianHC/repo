b203f49a682340556aca6cb2dc478aade048bdaf
VivianHC <52123322+VivianHC@users.noreply.github.com>
2019-11-17 21:19:27 +0800

    太不容易了

---
+++ phase2/DDL/create_tbl_students.sql
@@ -0,0 +1,22 @@
+/*==============================================================*/
+/* Table: course_info                                           */
+/*==============================================================*/
+create table course_info
+(
+   cid                  char(20) not null,
+   c_name               char(50) not null,
+   introduce            char(255) not null,
+   primary key (cid)
+);
+
+alter table course_info comment '所有课程的基本信息表';
+
+create table classes_info
+(
+   class_code           char(10) not null,
+   tid                  char(10) not null,
+   class_name           char(20) not null,
+   class_grade          int not null,
+   class_no             int not null,
+   primary key (class_code)
+);
