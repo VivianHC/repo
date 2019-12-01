560840e004b976b357f5901a4c31c3ed0ef74ed6
VivianHC <52123322+VivianHC@users.noreply.github.com>
2019-12-01 19:27:46 +0800

    中途提交

--- phase2/init_data/pkg_init_data/init_teachers.py
+++ phase2/init_data/pkg_init_data/init_teachers.py
@@ -10,8 +10,8 @@ db=MyDB()
 tbl_name='teacher_info'
 is_teacher=True
 is_man=1
-sql='DELETE FROM %s'% tbl_name
 '''
+sql='DELETE FROM %s'% tbl_name
 db.cursor.execute(sql)
 db.commit()
 '''
@@ -30,4 +30,22 @@ sql += '"%s","%s","%s",%d,"%s",%s' % (
 sql +=')'
 print(name)
 db.cursor.execute(sql)
+db.commit()
+
+for idx in range(100):
+    is_man=random.randint(1,2)
+    tid=gwn()
+    class_code=''
+    name=pfnbs(is_man)
+    sex=is_man
+    birthday=gbd(is_teacher)
+    in_charge=False
+
+    sql='INSERT INTO %s '%tbl_name
+    sql+='(tid,class_code,t_name,t_sex,t_birthday,in_charge)'
+    sql+='VALUES('
+    sql += '"%s","%s","%s",%d,"%s",%s' % (
+        tid, class_code, name, sex, birthday, in_charge)
+    sql+=')'
+    db.cursor.execute(sql)
 db.commit()
